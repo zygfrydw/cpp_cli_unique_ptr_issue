b2d5803ba18d09e081353c6fdc8071649f3bfc5a
Zygfryd Wieszok <z.wieszok@canon-ot.com.pl>
2018-11-08 18:58:58 +0100

    Initial commit

---
+++ Managed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wrapper;
+
+namespace Managed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------Start---------");
+
+            var wrapper = new WrapperClass();
+            wrapper.Run();
+            Console.WriteLine("---------End---------");
+        }
+    }
+}
